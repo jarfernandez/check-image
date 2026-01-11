0ca34c9a4ccc4f53f7926387ff8cbff9934e3bb9
Jose Arturo Fernandez Diaz <jarfernandez@gmail.com>
2026-01-11 22:42:54 +0100

    feat(registry): add command to validate trusted image registries

---
+++ cmd/check-image/commands/registry.go
@@ -0,0 +1,61 @@
+package commands
+
+import (
+	"check-image/internal/imageutil"
+	"check-image/internal/registry"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var registryPolicy string
+
+var registryCmd = &cobra.Command{
+	Use:   "registry image",
+	Short: "Validate that the image registry is trusted",
+	Long: `Validate that the image registry is trusted.
+The 'image' argument should be the name of a container image.`,
+	Example: `  check-image registry ghcr.io/kubernetes-sigs/kind/node --registry-policy registry-policy.json
+  check-image registry ghcr.io/kubernetes-sigs/kind/node --registry-policy registry-policy.yaml`,
+	Args: cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		if err := runRegistry(args[0]); err != nil {
+			return fmt.Errorf("check registry operation failed: %w", err)
+		}
+
+		return nil
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(registryCmd)
+	registryCmd.Flags().StringVarP(&registryPolicy, "registry-policy", "r", "", "Registry policy file (JSON or YAML)")
+	registryCmd.MarkFlagRequired("registry-policy")
+}
+
+func runRegistry(imageName string) error {
+	fmt.Printf("Checking registry of image %s\n", imageName)
+
+	imageRegistry, err := imageutil.GetImageRegistry(imageName)
+	if err != nil {
+		return fmt.Errorf("unable to get image registry: %w", err)
+	}
+
+	fmt.Printf("Image registry: %s\n", imageRegistry)
+
+	policy, err := registry.LoadRegistryPolicy(registryPolicy)
+	if err != nil {
+		return fmt.Errorf("unable to load registry policy: %w", err)
+	}
+
+	if policy.IsRegistryAllowed(imageRegistry) {
+		fmt.Printf("Registry %s is trusted\n", imageRegistry)
+		if Result != ValidationFailed {
+			Result = ValidationSucceeded
+		}
+	} else {
+		fmt.Printf("Registry %s is not trusted\n", imageRegistry)
+		Result = ValidationFailed
+	}
+
+	return nil
+}
